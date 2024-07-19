73962b1d3ff55f3b1302a0b8375429a3c9c01681
Alberto <garcia.lara.alberto@gmail.com>
2024-07-19 12:37:55 +0200

    Added Player component displayed in the game board section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import Player from "./Components/Player"
+
 
 function App() {
   
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id="game-container">
+        GAME BOARD
+        <ol id="players">
+         <Player name="Player 1" symbol="X"/>
+         <Player name="Player 2" symbol="O"/>
+        </ol>
+      </div>
+      <h1>React Tic-Tac-Toe</h1>
+    </main>
   )
 }
 
