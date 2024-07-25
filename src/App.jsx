792b4b5d35731d03a509ab9edfecb2026075b88a
Alberto <garcia.lara.alberto@gmail.com>
2024-07-25 10:37:39 +0200

    Makes the rematch button work

--- src/App.jsx
+++ src/App.jsx
@@ -54,6 +54,10 @@ function App() {
     })
   }
 
+  function handleRestart(){
+    setGameTurns([])
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -61,7 +65,9 @@ function App() {
          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/> 
         </ol>
-        {(winner || hasDraw) && <GameOver winner = {winner} />}
+        {(winner || hasDraw) && (
+          <GameOver winner = {winner} onRestart={handleRestart}/>
+          )}
         <GameBoard onSelectSquare={handleSelectSquare}
           board={gameBoard}
         />
