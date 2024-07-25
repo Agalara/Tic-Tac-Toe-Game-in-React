340a7d73a5833559581fa52960740d154abdf54b
Alberto <garcia.lara.alberto@gmail.com>
2024-07-25 09:57:09 +0200

    Moves initial gameboard and turns creation from GameBoard to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import Player from "./Components/Player"
 import GameBoard from "./Components/GameBoard"
 import Log from "./Components/Log"
 import { useState } from "react"
+import { WINNING_COMBINATIONS } from "./winning-combinations"
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+]
 
 function deriveActivePlayer (gameTurns){
   let currentPlayer = 'X'
@@ -14,6 +21,20 @@ function deriveActivePlayer (gameTurns){
 function App() {
   const [gameTurns, setGameTurns] = useState([])
   const activePlayer = deriveActivePlayer(gameTurns)
+
+  let gameBoard = initialGameBoard
+
+  for (const turn of gameTurns) {
+    const {square, player } = turn
+    const {row, col} = square
+    gameBoard[row][col] = player
+  }
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column]
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column]
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column]
+  }
 
   function handleSelectSquare (rowIndex, colIndex) {
     setGameTurns(prevTurns => {
@@ -34,7 +55,7 @@ function App() {
          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/> 
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare}
-          turns={gameTurns}
+          board={gameBoard}
         />
       </div>
       <h1>React Tic-Tac-Toe</h1>
