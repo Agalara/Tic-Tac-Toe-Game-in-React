09942c3851dc51d141a56b1f3ce29168c6b039f0
Alberto <garcia.lara.alberto@gmail.com>
2024-07-25 10:10:07 +0200

    Adds a check for a winning condition and a message to the winer

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,8 @@ function deriveActivePlayer (gameTurns){
 function App() {
   const [gameTurns, setGameTurns] = useState([])
   const activePlayer = deriveActivePlayer(gameTurns)
-
   let gameBoard = initialGameBoard
+  let winner = null
 
   for (const turn of gameTurns) {
     const {square, player } = turn
@@ -34,6 +34,10 @@ function App() {
     const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column]
     const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column]
     const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column]
+
+    if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol){
+      winner = firstSquareSymbol
+    }
   }
 
   function handleSelectSquare (rowIndex, colIndex) {
@@ -54,6 +58,7 @@ function App() {
          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/> 
         </ol>
+        {winner && <p> You won, {winner}! </p>}
         <GameBoard onSelectSquare={handleSelectSquare}
           board={gameBoard}
         />
